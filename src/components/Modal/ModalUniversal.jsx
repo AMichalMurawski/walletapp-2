435604965e1e79b45c71bdcf33e96ae177dc167d
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-30 00:06:55 +0200

    Create ModalUniversal.jsx

---
+++ src/components/Modal/ModalUniversal.jsx
@@ -0,0 +1,21 @@
+import s from './ModalUniversal.module.scss';
+import { createPortal } from 'react-dom';
+import { BsXLg } from 'react-icons/bs';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const ModalUniversal = prop => {
+  return createPortal(
+    <div className={s.modal__backdrop} onClick={prop.onClick} type="flipInX">
+      <div className={s.modal__content}>
+        <button className={s.btnClose} onClick={prop.onClose}>
+          <BsXLg className={s.btnClose__icon} />
+        </button>
+        {prop.children}
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+export default ModalUniversal;
