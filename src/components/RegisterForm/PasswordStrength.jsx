930481a9e1d884140caf7959f11fba867c4610f6
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-05-02 21:15:25 +0200

    Create PasswordStrength.jsx

---
+++ src/components/RegisterForm/PasswordStrength.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import zxcvbn from "zxcvbn";
+import s from './PasswordStrength.module.scss';
+
+const PasswordStrength = ({ password }) => {
+  const result = zxcvbn(password);
+  const num = (result.score * 100) / 4;
+
+  const progressColor = () => {
+    switch (result.score) {
+      case 0:
+        return 'none';
+      case 1:
+        return '#FF6596';
+      case 2:
+        return '#FED057';
+      case 3:
+        return '#80d564';
+      case 4:
+        return '#24CCA7';
+      default:
+        return 'none';
+    }
+  }
+
+  const changePasswordColor = () => ({
+    width: `${num}%`,
+    background: progressColor()
+  })
+
+  return (
+    <>
+      <div className={s.progressBar}>
+        <div className={s.progress} style={changePasswordColor()}></div>
+      </div>
+    </>
+)
+}
+
+export default PasswordStrength;
