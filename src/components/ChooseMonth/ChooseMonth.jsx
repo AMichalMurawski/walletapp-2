7a2528d6964cdf2b7cc060f609eaa9beb95cfd3e
Patrycja Lamik <patrycjalamik94@gmail.com>
2023-05-04 20:10:03 +0200

    statistics

--- src/components/ChooseMonth/ChooseMonth.jsx
+++ src/components/ChooseMonth/ChooseMonth.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState } from 'react';
 import css from './ChooseMonth.module.scss';
 
-function ChooseMonth ({selected, setSelected}) {
+const ChooseMonth = ({selected, setSelected}) => {
 
     const [isActive, setIsActive] = useState(false);
     const options = ['January', "February", 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -10,12 +10,10 @@ function ChooseMonth ({selected, setSelected}) {
 
     return (
         <div className={css.dropdown}>
-            <div>
                     <div className={css.dropdownBtn} onClick={ e => setIsActive(!isActive)}> 
-                    {selected}
+                        {selected}
                     <div className={css.caret}></div>
-                </div>
-            </div>
+        </div>
 
             {isActive && (
                 <div className={css.dropdownContent} >
