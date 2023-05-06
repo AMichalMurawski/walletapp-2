ef206797c4faca1b542dad2fcddc0fa6432b1b2e
Michał Murawski <m.a.murawski@wp.pl>
2023-05-06 21:59:59 +0200

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,15 +1,14 @@
-import { Helmet } from "react-helmet";
-import { useState } from "react";
-import Chart from "../components/Chart/Chart";
-import ChooseMonth from "../components/ChooseMonth/ChooseMonth";
-import ChooseYear from "../components/ChooseYear/ChooseYear";
-import LegendChart from "../components/LegendChart/LegendChart";
+import { Helmet } from 'react-helmet';
+import { useState } from 'react';
+import Chart from '../components/Chart/Chart';
+import ChooseMonth from '../components/ChooseMonth/ChooseMonth';
+import ChooseYear from '../components/ChooseYear/ChooseYear';
+import LegendChart from '../components/LegendChart/LegendChart';
 import css from './DashboardPage.module.scss';
 
 const DashboardPage = () => {
-
-  const [selected, setSelected] = useState("Month");
-  const [selectedYear, setSelectedYear] = useState("Year");
+  const [selected, setSelected] = useState('Month');
+  const [selectedYear, setSelectedYear] = useState('Year');
 
   return (
     <>
@@ -17,16 +16,15 @@ const DashboardPage = () => {
         <title>walletapp</title>
       </Helmet>
       <div className={css.statisticsContainer}>
-            <Chart />
-             <div className={css.statisticsPage}>
-              <div  className={css.statisticsSelect} >
-                <ChooseMonth selected={selected} setSelected={setSelected}/>
-                <ChooseYear selected={selectedYear} setSelected={setSelectedYear}/>
-              </div>
-              <LegendChart/>
-             </div>
+        <Chart />
+        <div className={css.statisticsPage}>
+          <div className={css.statisticsSelect}>
+            <ChooseMonth selected={selected} setSelected={setSelected} />
+            <ChooseYear selected={selectedYear} setSelected={setSelectedYear} />
+          </div>
+          <LegendChart />
+        </div>
       </div>
-
     </>
   );
 };
